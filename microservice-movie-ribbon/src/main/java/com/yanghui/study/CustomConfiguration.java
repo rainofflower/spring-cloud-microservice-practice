bb3bac8c6fd9ff3e80274937c1a12b45a237c764
rainofflower <2462345495@qq.com>
2018-11-02 00:21:42 +0800

    ribbon基本使用、改造microservice-userbase服务

---
+++ microservice-movie-ribbon/src/main/java/com/yanghui/study/CustomConfiguration.java
@@ -0,0 +1,17 @@
+package com.yanghui.study;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.netflix.loadbalancer.BestAvailableRule;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+
+@Configuration
+public class CustomConfiguration {
+	
+	@Bean
+	public IRule ribbonRule() {
+		return new RandomRule();  //BestAvailableRule();
+	}
+}
---
+++ microservice-movie-ribbon/src/main/java/com/yanghui/study/ExcludeAnnotation.java
@@ -0,0 +1,5 @@
+package com.yanghui.study;
+
+public @interface ExcludeAnnotation {
+
+}
