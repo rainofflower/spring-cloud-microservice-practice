bd3bcd04f58c88ac56b7ce0cb9b472fe426c19df
rainofflower <2462345495@qq.com>
2018-11-06 19:44:49 +0800

    注册到Eureka Server

--- microservice-hystrix-dashboard/src/main/java/com/yanghui/study/DashBoardApplication.java
+++ microservice-hystrix-dashboard/src/main/java/com/yanghui/study/DashBoardApplication.java
@@ -2,9 +2,11 @@ package com.yanghui.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableHystrixDashboard
 public class DashBoardApplication {
 	public static void main(String[] args) {
--- microservice-turbine/src/main/java/com/yanghui/study/TurbineApplication.java
+++ microservice-turbine/src/main/java/com/yanghui/study/TurbineApplication.java
@@ -2,9 +2,11 @@ package com.yanghui.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableTurbine
 public class TurbineApplication {
 	public static void main(String[] args) {
